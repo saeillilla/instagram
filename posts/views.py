559c9e5489dfb6f14ff8da0d6b6af52b1b9e571b
Student <Student>
2021-02-10 15:17:39 +0530

    add like and comment ajax

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render,redirect
 from .models import Post,Like
 from .forms import Add_Post
+from django.http import JsonResponse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(request):
-    posts = Post.objects.all()
-    # likes = Like.objects.filter(post=post).count()
-    return render(request,'posts/home.html',{'posts':posts})
+    posts = Post.objects.all().prefetch_related("post_likes", 'post_comments', 'post_comments__user').select_related("user")
+    likes = Like.objects.filter(post__in=posts, user=request.user).values_list("post", flat=True)
+
+    def is_liked_add(p):
+        p.is_liked = p.id in likes
+        return p
+
+    posts = list(map(is_liked_add, posts))
+    return render(request, "posts/home.html", {"posts": posts})
     
 def log_out(request):
     logout(request)
@@ -30,13 +37,15 @@ def addPosts(request):
 def add_likes(request, post_id):
     post = Post.objects.get(id=post_id)
     user = request.user
-
+    response={}
     try:
         already_liked = Like.objects.get(post=post, user=user)
         already_liked.delete()
+        response['status']='dislike'
     except Like.DoesNotExist:
         Like.objects.create(post=post, user=user)
-
-    return redirect('/posts/home/')
+        response['status']='like'
+    response['count']=Like.objects.filter(post=post).count()
+    return JsonResponse(response)
 
     
